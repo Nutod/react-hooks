153124c7ce800e76e6584bb13b6c23463ac88787
Nutod <alukodotun@gmail.com>
2021-03-27 08:07:06 +0700

    Added new logic to context

--- react-hooks-course/app/contexts/theme.js
+++ react-hooks-course/app/contexts/theme.js
@@ -2,5 +2,22 @@ import React from 'react'
 
 const { Consumer, Provider } = React.createContext()
 
+export const ThemeContext = React.createContext()
+
+export function ThemeContextProvider({ children }) {
+  const value = {}
+  return <ThemeContext.Provider>{children}</ThemeContext.Provider>
+}
+
+export function useTheme() {
+  const context = React.useContext(ThemeContext)
+
+  if (!context) {
+    throw new Error('Something went wrong')
+  }
+
+  return context
+}
+
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
