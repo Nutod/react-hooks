249a217399080ed4466407e161ad040b13d8e9ca
Nutod <alukodotun@gmail.com>
2021-04-02 15:23:36 +0700

    Updated context value

--- react-hooks-course/app/contexts/theme.tsx
+++ react-hooks-course/app/contexts/theme.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 
-interface ThemeInterface {
-  theme: string
-}
-
-export const ThemeContext = React.createContext<null | ThemeInterface>({
+export const ThemeContext = React.createContext<{ theme: string }>({
   theme: 'light',
 })
 
