103d177062ac2c1729e7b824a518c068eed6859c
Nutod <alukodotun@gmail.com>
2021-04-04 07:30:36 +0700

    Updated theme context provider

--- react-hooks-course-curriculum/app/contexts/theme.js
+++ react-hooks-course-curriculum/app/contexts/theme.js
@@ -1,5 +1,31 @@
 import React from 'react'
 
+export const ThemeContext = React.createContext()
+ThemeContext.displayName = 'ThemeContext'
+
+export function ThemeContextProvider({ children }) {
+  const [theme, setTheme] = React.useState('light')
+
+  const toggleTheme = () => {
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
+  }
+
+  const value = {
+    theme,
+    toggleTheme,
+  }
+
+  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+}
+
+export function useTheme() {
+  const context = React.useContext(ThemeContext)
+
+  if (!context) throw new Error('Context is not defined')
+
+  return context
+}
+
 const { Consumer, Provider } = React.createContext()
 
 export const ThemeConsumer = Consumer
