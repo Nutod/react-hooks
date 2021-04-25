d33cb703c00815a31f34c272d8cbdf97c8e43117
Nutod <alukodotun@gmail.com>
2021-04-25 20:15:07 +0700

    Added new context provider

--- react-hooks-course-curriculum/app/app.js
+++ react-hooks-course-curriculum/app/app.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider } from './contexts/theme'
---
+++ react-hooks-course-curriculum/app/hooks/useTheme.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import constate from 'constate/dist/ts/src'
+
+function useTheme() {
+  const [theme, setTheme] = React.useState('light')
+
+  const toggle = React.useCallback(
+    () =>
+      setTheme(previousTheme => (previousTheme === 'light' ? 'dark' : 'light')),
+    []
+  )
+
+  return { theme, toggle }
+}
+
+export const [ThemeContextProvider, useThemeContext] = constate(useTheme)
