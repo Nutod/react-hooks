39a9597ac5951c50847443d0e60f908adb8e5ff4
Nutod <alukodotun@gmail.com>
2021-04-25 20:55:12 +0700

    Moved files around

--- react-hooks-course-curriculum/webpack.config.js
+++ react-hooks-course-curriculum/webpack.config.js
@@ -3,28 +3,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './app/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
-    publicPath: '/'
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
+      { test: /\.(js|ts|tsx)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
   },
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
+      template: 'app/index.html',
     }),
-    new CopyPlugin([
-      { from : '_redirects' }
-    ])
+    new CopyPlugin([{ from: '_redirects' }]),
   ],
   devServer: {
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 }
