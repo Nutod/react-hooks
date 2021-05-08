5714499f36e246bad5d4b9d4eb8a3472847bf3b3
Nutod <alukodotun@gmail.com>
2021-05-09 01:46:15 +0700

    Added new context function

---
+++ react-hooks-course-curriculum/app/contexts/useTheme.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import constate from 'constate'
+
+function useTheme() {
+  const [theme, setTheme] = React.useState('light')
+
+  const toggleTheme = React.useCallback(
+    () =>
+      setTheme(previousTheme => (previousTheme === 'light' ? 'dark' : 'light')),
+    []
+  )
+
+  return { theme, toggleTheme }
+}
+
+export const [ThemeContextProvider, useThemeContext] = constate(useTheme)
