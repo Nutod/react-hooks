51a40dccae7f8bf9273aee1614f644b128b4ab5d
Nutod <alukodotun@gmail.com>
2021-05-22 08:09:31 +0700

    Added new context object

---
+++ react-hooks-course/app/contexts/useTheme.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import constate from 'constate'
+
+function useTheme({ defaultTheme = 'light' }) {
+  const [theme, setTheme] = React.useState(defaultTheme)
+
+  const toggleTheme = React.useCallback(
+    () => setTheme(prev => (prev === 'light' ? 'dark' : 'light')),
+    []
+  )
+
+  return { theme, toggleTheme }
+}
+
+export const [ThemeContextProvider, useThemeContext] = constate(useTheme)
