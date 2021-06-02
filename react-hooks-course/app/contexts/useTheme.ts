78e3980afc70092851447d88265cc82b03795ec6
Nutod <alukodotun@gmail.com>
2021-06-02 15:07:44 +0700

    Updated constate theme context usage

--- react-hooks-course/app/contexts/useTheme.ts
+++ react-hooks-course/app/contexts/useTheme.ts
@@ -1,8 +1,8 @@
 import React from 'react'
 import constate from 'constate'
 
-function useTheme() {
-  const [theme, setTheme] = React.useState('light')
+function useTheme({ defaultTheme = 'light' }) {
+  const [theme, setTheme] = React.useState(defaultTheme)
 
   const toggleTheme = React.useCallback(() => {
     setTheme(previousTheme => (previousTheme === 'light' ? 'dark' : 'light'))
