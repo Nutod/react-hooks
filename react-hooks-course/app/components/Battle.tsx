0af107265db355e003f2276535310f885cf7a217
Nutod <alukodotun@gmail.com>
2021-06-02 15:26:51 +0700

    Updated PlayerPreview component

--- react-hooks-course/app/components/Battle.tsx
+++ react-hooks-course/app/components/Battle.tsx
@@ -95,30 +95,36 @@ PlayerInput.propTypes = {
   label: PropTypes.string.isRequired,
 }
 
-function PlayerPreview({ username, onReset, label }) {
+function PlayerPreview({
+  username,
+  onReset,
+  label,
+}: {
+  username: string
+  onReset: () => void
+  label: string
+}) {
+  const { theme } = useThemeContext()
+
   return (
-    <ThemeConsumer>
-      {({ theme }) => (
-        <div className="column player">
-          <h3 className="player-label">{label}</h3>
-          <div className={`row bg-${theme}`}>
-            <div className="player-info">
-              <img
-                className="avatar-small"
-                src={`https://github.com/${username}.png?size=200`}
-                alt={`Avatar for ${username}`}
-              />
-              <a href={`https://github.com/${username}`} className="link">
-                {username}
-              </a>
-            </div>
-            <button className="btn-clear flex-center" onClick={onReset}>
-              <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
-            </button>
-          </div>
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className={`row bg-${theme}`}>
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
         </div>
-      )}
-    </ThemeConsumer>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
   )
 }
 
