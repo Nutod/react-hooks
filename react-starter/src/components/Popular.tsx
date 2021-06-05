9bd6e0402846661ac542bce853f56299e4ccb5e0
Nutod <alukodotun@gmail.com>
2021-06-05 21:20:16 +0700

    Updated Popular Component Background

--- react-starter/src/components/Popular.tsx
+++ react-starter/src/components/Popular.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 type Language = 'All' | 'Javascript' | 'Python' | 'Ruby' | 'CSS' | 'Java'
+
+const LanguagesList = styled.ul`
+  background: pink;
+
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+`
 
 export default function Popular() {
   const languages: Language[] = ['All', 'Javascript', 'Python', 'Ruby', 'CSS', 'Java']
 
   return (
     <div>
-      <ul>
+      <LanguagesList>
         {languages.map(language => (
           <li>
             <a>{language}</a>
           </li>
         ))}
-      </ul>
+      </LanguagesList>
     </div>
   )
 }
