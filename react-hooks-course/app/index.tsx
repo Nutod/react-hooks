f246df9b80e1c8b9dc61d58833fa1a8880d17640
Nutod <alukodotun@gmail.com>
2021-06-05 12:06:01 +0700

    Updated app component to use new theme context

--- react-hooks-course/app/index.tsx
+++ react-hooks-course/app/index.tsx
@@ -2,5 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './app'
+import { ThemeContextProvider } from './contexts/useTheme'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>,
+  document.getElementById('app')
+)
