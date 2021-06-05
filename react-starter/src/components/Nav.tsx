73687a41e0f5e49e43b156ef8bb116ccb5062586
Nutod <alukodotun@gmail.com>
2021-06-05 21:23:14 +0700

    Added Nav component

---
+++ react-starter/src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavWrapper = styled.div``
+
+export default function Nav() {
+  return (
+    <NavWrapper>
+      <div>
+        <a href="">Popular</a>
+        <a href="">Battle</a>
+      </div>
+      <div>🔆🔦</div>
+    </NavWrapper>
+  )
+}
+// 🔆🔦
