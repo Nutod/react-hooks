a1edf99865aa9204c03d37d94a6f34f42f520f53
Nutod <alukodotun@gmail.com>
2021-06-05 12:00:35 +0700

    Updated Hover component to use Typescript

--- react-hooks-course/app/components/Hover.tsx
+++ react-hooks-course/app/components/Hover.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-export default class Hover extends React.Component {
+export default class Hover extends React.Component<
+  { children: (param: boolean) => ReactNode },
+  {
+    hovering: boolean
+  }
+> {
   state = { hovering: false }
+
   mouseOver = () => this.setState({ hovering: true })
   mouseOut = () => this.setState({ hovering: false })
-  render () {
+
+  render() {
     return (
       <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
         {this.props.children(this.state.hovering)}
