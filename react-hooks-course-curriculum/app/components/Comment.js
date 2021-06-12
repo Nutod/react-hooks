566166b7995de8f9902dedc664062ada2a8ccbf6
Nutod <alukodotun@gmail.com>
2021-06-12 14:38:30 +0700

    Initialised new project

--- react-hooks-course-curriculum/app/components/Comment.js
+++ react-hooks-course-curriculum/app/components/Comment.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import PostMetaInfo from './PostMetaInfo'
-import { PostType } from './Post'
 
-export default function Comment({ comment }: { comment: PostType }) {
+export default function Comment ({ comment }) {
   return (
-    <div className="comment">
+    <div className='comment'>
       <PostMetaInfo
         comment={true}
         by={comment.by}
         time={comment.time}
         id={comment.id}
       />
-      <p dangerouslySetInnerHTML={{ __html: comment.text }} />
+      <p dangerouslySetInnerHTML={{__html: comment.text}} />
     </div>
   )
 }
