2c302d93534b064ee046cebe1083a5cae18a229d
Nutod <alukodotun@gmail.com>
2021-06-21 12:17:48 +0700

    Added new app component file

--- react-hooks-course/app/index.tsx
+++ react-hooks-course/app/index.tsx
@@ -1,41 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { ThemeProvider } from './contexts/theme'
-import Nav from './components/Nav'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Loading from './components/Loading'
+import { ThemeContextProvider } from './contexts/theme'
+import App from './app'
 
-const Popular = React.lazy(() => import('./components/Popular'))
-const Battle = React.lazy(() => import('./components/Battle'))
-const Results = React.lazy(() => import('./components/Results'))
-
-function App() {
-  const [theme, setTheme] = React.useState('light')
-  const toggleTheme = React.useCallback(() => {
-    setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
-  }, [])
-
-  return (
-    <Router>
-      <ThemeProvider value={{ theme, toggleTheme }}>
-        <div className={theme}>
-          <div className="container">
-            <Nav />
-
-            <React.Suspense fallback={<Loading />}>
-              <Switch>
-                <Route exact path="/" component={Popular} />
-                <Route exact path="/battle" component={Battle} />
-                <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>404</h1>} />
-              </Switch>
-            </React.Suspense>
-          </div>
-        </div>
-      </ThemeProvider>
-    </Router>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>,
+  document.getElementById('app')
+)
