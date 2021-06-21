f9e7603cf45e47ff7ab31b9a864f79a0cad98545
Nutod <alukodotun@gmail.com>
2021-06-21 14:29:02 +0700

    Updated User component

--- react-hooks-course-curriculum/app/components/User.tsx
+++ react-hooks-course-curriculum/app/components/User.tsx
@@ -4,61 +4,74 @@ import { fetchUser, fetchPosts } from '../utils/api'
 import Loading from './Loading'
 import { formatDate } from '../utils/helpers'
 import PostsList from './PostsList'
+import { IPost } from './Post'
 
-export default class User extends React.Component {
-  state = {
-    user: null,
-    loadingUser: true,
-    posts: null,
-    loadingPosts: true,
-    error: null,
-  }
-  componentDidMount() {
-    const { id } = queryString.parse(this.props.location.search)
+export interface IUser {
+  id: number
+  karma: number
+  created: number
+  about: string
+}
+
+export default function User({ location }: { location: { search: string } }) {
+  const [user, setUser] = React.useState<null | IUser>(null)
+  const [loadingUser, setLoadingUser] = React.useState(true)
+  const [posts, setPosts] = React.useState<null | IPost[]>(null)
+  const [loadingPosts, setLoadingPosts] = React.useState(true)
+  const [error, setError] = React.useState(null)
+
+  React.useEffect(() => {
+    const { id } = queryString.parse(location.search) as { id: string }
 
     fetchUser(id)
-      .then((user) => {
-        this.setState({ user, loadingUser: false})
+      .then(user => {
+        setUser(user)
+        setLoadingUser(false)
 
         return fetchPosts(user.submitted.slice(0, 30))
       })
-      .then((posts) => this.setState({
-        posts,
-        loadingPosts: false,
-        error: null
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
-        loadingUser: false,
-        loadingPosts: false
-      }))
-  }
-  render() {
-    const { user, posts, loadingUser, loadingPosts, error } = this.state
+      .then(posts => {
+        setPosts(posts)
+        setLoadingPosts(false)
+        setError(null)
+      })
+      .catch(({ message }) => {
+        setError(message)
+        setLoadingUser(false)
+        setLoadingPosts(false)
+      })
+  }, [])
 
-    if (error) {
-      return <p className='center-text error'>{error}</p>
-    }
-
-    return (
-      <React.Fragment>
-        {loadingUser === true
-          ? <Loading text='Fetching User' />
-          : <React.Fragment>
-              <h1 className='header'>{user.id}</h1>
-              <div className='meta-info-light'>
-                <span>joined <b>{formatDate(user.created)}</b></span>
-                <span>has <b>{user.karma.toLocaleString()}</b> karma</span>
-              </div>
-              <p dangerouslySetInnerHTML={{__html: user.about}} />
-            </React.Fragment>}
-        {loadingPosts === true
-          ? loadingUser === false && <Loading text='Fetching posts'/>
-          : <React.Fragment>
-              <h2>Posts</h2>
-              <PostsList posts={posts} />
-            </React.Fragment>}
-      </React.Fragment>
-    )
+  if (error) {
+    return <p className="center-text error">{error}</p>
   }
+
+  return (
+    <React.Fragment>
+      {loadingUser === true || !user ? (
+        <Loading text="Fetching User" />
+      ) : (
+        <React.Fragment>
+          <h1 className="header">{user.id}</h1>
+          <div className="meta-info-light">
+            <span>
+              joined <b>{formatDate(user.created)}</b>
+            </span>
+            <span>
+              has <b>{user.karma.toLocaleString()}</b> karma
+            </span>
+          </div>
+          <p dangerouslySetInnerHTML={{ __html: user.about }} />
+        </React.Fragment>
+      )}
+      {loadingPosts === true || !posts ? (
+        loadingUser === false && <Loading text="Fetching posts" />
+      ) : (
+        <React.Fragment>
+          <h2>Posts</h2>
+          <PostsList posts={posts} />
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  )
 }
