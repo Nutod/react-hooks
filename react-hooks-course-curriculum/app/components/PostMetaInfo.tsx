8f1363a2ee5090332f8a8524ea884b94db1d7f63
Nutod <alukodotun@gmail.com>
2021-06-21 14:01:10 +0700

    Updated comment component

--- react-hooks-course-curriculum/app/components/PostMetaInfo.tsx
+++ react-hooks-course-curriculum/app/components/PostMetaInfo.tsx
@@ -9,11 +9,13 @@ export default function PostMetaInfo({
   time,
   id,
   descendants,
+  comment,
 }: {
   by: string
   time: number
   id: number | string
-  descendants: number
+  descendants?: number
+  comment?: boolean
 }) {
   const { theme } = useThemeContext()
 
