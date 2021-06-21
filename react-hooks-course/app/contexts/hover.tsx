f4ac75a583cdb0f15ef228ad3a0ab60573e43a48
Nutod <alukodotun@gmail.com>
2021-06-21 13:16:30 +0700

    Added new context hook

---
+++ react-hooks-course/app/contexts/hover.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from 'react'
+
+interface IHoverContext {
+  hovering: boolean
+  mouseOn: () => void
+  mouseOut: () => void
+}
+
+function useHover() {
+  const [hovering, setHovering] = React.useState(false)
+
+  const mouseOn = React.useCallback(() => {
+    setHovering(true)
+  }, [])
+
+  const mouseOut = React.useCallback(() => {
+    setHovering(false)
+  }, [])
+
+  return { hovering, mouseOn, mouseOut }
+}
+
+const HoverContext = React.createContext({} as IHoverContext)
+
+export function ThemeContextProvider({ children }: { children: ReactNode }) {
+  const { hovering, mouseOn, mouseOut } = useHover()
+
+  return (
+    <HoverContext.Provider value={{ hovering, mouseOn, mouseOut }}>
+      {children}
+    </HoverContext.Provider>
+  )
+}
+
+export function useHoverContext() {
+  const context = React.useContext(HoverContext)
+
+  if (!context) throw new Error('Context is not defined')
+
+  return context
+}
