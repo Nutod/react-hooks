56192762ddf2315a91c7e58916a5c8468f66b074
Nutod <alukodotun@gmail.com>
2021-06-26 14:13:49 +0700

    Updated Results component

--- react-hooks-course/app/components/Results.tsx
+++ react-hooks-course/app/components/Results.tsx
@@ -13,8 +13,9 @@ import Loading from './Loading'
 import Tooltip from './Tooltip'
 import queryString from 'query-string'
 import { Link } from 'react-router-dom'
+import { IOwner } from './Popular'
 
-export interface IProfile {
+export interface IProfile extends IOwner {
   name: string
   company: string
   location: string
@@ -61,71 +62,108 @@ ProfileList.propTypes = {
   profile: PropTypes.object.isRequired,
 }
 
-export default class Results extends React.Component {
-  state = {
-    winner: null,
-    loser: null,
-    error: null,
-    loading: true,
-  }
-  componentDidMount() {
-    const { playerOne, playerTwo } = queryString.parse(
-      this.props.location.search
-    )
+export interface IPlayer {
+  score: number
+  profile: IProfile
+}
+
+type StateType = {
+  winner: null | IPlayer
+  loser: null | IPlayer
+  error: string | null
+  loading: boolean
+}
+
+type ResultsType = {
+  type: 'results'
+  players: IPlayer[]
+}
+
+type ErrorType = {
+  type: 'error'
+  message: string
+}
+
+type ActionType = ResultsType | ErrorType
+
+export default function Results({
+  location,
+}: {
+  location: { search: string }
+}) {
+  const [{ winner, loser, error, loading }, dispatch] = React.useReducer(
+    (state: StateType, action: ActionType): StateType => {
+      switch (action.type) {
+        case 'results':
+          return {
+            ...state,
+            winner: action.players[0],
+            loser: action.players[1],
+            error: null,
+            loading: false,
+          }
+        case 'error':
+          return { ...state, error: action.message, loading: false }
+
+        default:
+          return state
+      }
+    },
+    {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    }
+  )
+
+  React.useEffect(() => {
+    const { playerOne, playerTwo } = queryString.parse(location.search) as {
+      [id: string]: string
+    }
 
     battle([playerOne, playerTwo])
       .then(players => {
-        this.setState({
-          winner: players[0],
-          loser: players[1],
-          error: null,
-          loading: false,
-        })
+        dispatch({ type: 'results', players })
       })
       .catch(({ message }) => {
-        this.setState({
-          error: message,
-          loading: false,
-        })
+        dispatch({ type: 'error', message })
       })
+  }, [])
+
+  if (loading === true || !winner || !loser) {
+    return <Loading text="Battling" />
   }
-  render() {
-    const { winner, loser, error, loading } = this.state
 
-    if (loading === true) {
-      return <Loading text="Battling" />
-    }
-
-    if (error) {
-      return <p className="center-text error">{error}</p>
-    }
-
-    return (
-      <React.Fragment>
-        <div className="grid space-around container-sm">
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Winner'}
-            subheader={`Score: ${winner.score.toLocaleString()}`}
-            avatar={winner.profile.avatar_url}
-            href={winner.profile.html_url}
-            name={winner.profile.login}
-          >
-            <ProfileList profile={winner.profile} />
-          </Card>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Loser'}
-            subheader={`Score: ${loser.score.toLocaleString()}`}
-            avatar={loser.profile.avatar_url}
-            name={loser.profile.login}
-            href={loser.profile.html_url}
-          >
-            <ProfileList profile={loser.profile} />
-          </Card>
-        </div>
-        <Link to="/battle" className="btn dark-btn btn-space">
-          Reset
-        </Link>
-      </React.Fragment>
-    )
+  if (error) {
+    return <p className="center-text error">{error}</p>
   }
+
+  return (
+    <React.Fragment>
+      <div className="grid space-around container-sm">
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Winner'}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ProfileList profile={winner.profile} />
+        </Card>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          name={loser.profile.login}
+          href={loser.profile.html_url}
+        >
+          <ProfileList profile={loser.profile} />
+        </Card>
+      </div>
+      <Link to="/battle" className="btn dark-btn btn-space">
+        Reset
+      </Link>
+    </React.Fragment>
+  )
 }
