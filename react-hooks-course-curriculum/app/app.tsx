6b734f2a9b7e513de76a5c55d2d1d9810f538af0
Nutod <alukodotun@gmail.com>
2021-07-10 19:50:31 +0700

    Added new context to app component

--- react-hooks-course-curriculum/app/app.tsx
+++ react-hooks-course-curriculum/app/app.tsx
@@ -3,30 +3,32 @@ import './index.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Loading from './components/Loading'
 import Nav from './components/Nav'
+import { useThemeContext } from './contexts/useTheme'
 
 const Posts = React.lazy(() => import('./components/Posts'))
 const Post = React.lazy(() => import('./components/Post'))
 const User = React.lazy(() => import('./components/User'))
 
-export class App extends React.Component {
-    return (
-      <Router>
-          <div className={this.state.theme}>
-            <div className="container">
-              <Nav />
+export default function App() {
+  const { theme } = useThemeContext()
 
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route exact path="/" render={() => <Posts type="top" />} />
-                  <Route path="/new" render={() => <Posts type="new" />} />
-                  <Route path="/post" component={Post} />
-                  <Route path="/user" component={User} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-      </Router>
-    )
-  
+  return (
+    <Router>
+      <div className={theme}>
+        <div className="container">
+          <Nav />
+
+          <React.Suspense fallback={<Loading />}>
+            <Switch>
+              <Route exact path="/" render={() => <Posts type="top" />} />
+              <Route path="/new" render={() => <Posts type="new" />} />
+              <Route path="/post" component={Post} />
+              <Route path="/user" component={User} />
+              <Route render={() => <h1>404</h1>} />
+            </Switch>
+          </React.Suspense>
+        </div>
+      </div>
+    </Router>
+  )
 }
