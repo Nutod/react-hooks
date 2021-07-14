1004b6c529e0c487c80794ceff0d11c4929f03d9
Nutod <alukodotun@gmail.com>
2021-07-14 15:34:11 +0700

    Added new api to theme file

--- react-hooks-course/app/contexts/theme.tsx
+++ react-hooks-course/app/contexts/theme.tsx
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 const { Consumer, Provider } = React.createContext()
 
+// Don't cause breaking changes
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+interface IThemeContext {
+  theme: 'light' | 'dark'
+  toggleTheme: () => void
+}
+
+const ThemeContext = React.createContext({} as IThemeContext)
+
+ThemeContext.displayName = 'ThemeContext'
+
+// {defaultTheme}: {defaultTheme: 'dark' | 'light'}
+
+export function ThemeContextProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
+  }, [])
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export function useTheme() {
+  const context = React.useContext(ThemeContext)
+
+  if (!context) throw new Error('Context is not defined')
+
+  return context
+}
