c642b927e9cb614fa6d243278c2f94ef717a0eb0
Nutod <alukodotun@gmail.com>
2021-07-15 13:15:40 +0700

    Added new context provider to app component

--- react-hooks-course-curriculum/app/app.tsx
+++ react-hooks-course-curriculum/app/app.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { ThemeProvider } from './contexts/theme'
+import { useThemeContext } from './contexts/theme'
 import Loading from './components/Loading'
 import Nav from './components/Nav'
 
@@ -10,36 +9,26 @@ const Posts = React.lazy(() => import('./components/Posts'))
 const Post = React.lazy(() => import('./components/Post'))
 const User = React.lazy(() => import('./components/User'))
 
-export default class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light',
-      }))
-    },
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className="container">
-              <Nav />
+export default function App() {
+  const { theme } = useThemeContext()
 
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route exact path="/" render={() => <Posts type="top" />} />
-                  <Route path="/new" render={() => <Posts type="new" />} />
-                  <Route path="/post" component={Post} />
-                  <Route path="/user" component={User} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
-    )
-  }
+  return (
+    <Router>
+      <div className={theme}>
+        <div className="container">
+          <Nav />
+
+          <React.Suspense fallback={<Loading />}>
+            <Switch>
+              <Route exact path="/" render={() => <Posts type="top" />} />
+              <Route path="/new" render={() => <Posts type="new" />} />
+              <Route path="/post" component={Post} />
+              <Route path="/user" component={User} />
+              <Route render={() => <h1>404</h1>} />
+            </Switch>
+          </React.Suspense>
+        </div>
+      </div>
+    </Router>
+  )
 }
