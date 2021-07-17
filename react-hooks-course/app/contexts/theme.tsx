023156e90613a4abecbd6505ed0506007d6125bb
Nutod <alukodotun@gmail.com>
2021-07-17 10:45:04 +0700

    Added new context to app component

--- react-hooks-course/app/contexts/theme.tsx
+++ react-hooks-course/app/contexts/theme.tsx
@@ -5,7 +5,12 @@ const { Consumer, Provider } = React.createContext()
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
 
-const ThemeContext = React.createContext({})
+const ThemeContext = React.createContext(
+  {} as {
+    theme: 'light' | 'dark'
+    toggleTheme: () => void
+  }
+)
 
 export function ThemeContextProvider({ children }: { children: ReactNode }) {
   const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
