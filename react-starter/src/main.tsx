4afa0876872c9b4b5030562a194c2fcbf67c4595
Nutod <alukodotun@gmail.com>
2021-07-20 12:42:35 +0700

    Added theme context api to main component

--- react-starter/src/main.tsx
+++ react-starter/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { GlobalStyles } from './styles/global'
+import { ThemeContextProvider } from './context/theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeContextProvider>
+      <GlobalStyles />
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
