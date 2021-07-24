0c3bffc08af1882eb7e43c1d3a7846a7b41bf910
Nutod <alukodotun@gmail.com>
2021-07-24 13:35:12 +0700

    Added annotations to api file

--- react-hooks-course/app/components/Results.tsx
+++ react-hooks-course/app/components/Results.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { battle } from '../utils/api'
-import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
+import {
+  FaCompass,
+  FaBriefcase,
+  FaUsers,
+  FaUserFriends,
+  FaCode,
+  FaUser,
+} from 'react-icons/fa'
 import Card from './Card'
 import PropTypes from 'prop-types'
 import Loading from './Loading'
@@ -8,17 +15,25 @@ import Tooltip from './Tooltip'
 import queryString from 'query-string'
 import { Link } from 'react-router-dom'
 
-function ProfileList ({ profile }) {
+export interface IProfile {
+  name: string
+  location: string
+  company: string
+  followers: number
+  following: number
+}
+
+function ProfileList({ profile }: { profile: IProfile }) {
   return (
-    <ul className='card-list'>
+    <ul className="card-list">
       <li>
-        <FaUser color='rgb(239, 115, 115)' size={22} />
+        <FaUser color="rgb(239, 115, 115)" size={22} />
         {profile.name}
       </li>
       {profile.location && (
         <li>
           <Tooltip text="User's location">
-            <FaCompass color='rgb(144, 115, 255)' size={22} />
+            <FaCompass color="rgb(144, 115, 255)" size={22} />
             {profile.location}
           </Tooltip>
         </li>
@@ -26,17 +41,17 @@ function ProfileList ({ profile }) {
       {profile.company && (
         <li>
           <Tooltip text="User's company">
-            <FaBriefcase color='#795548' size={22} />
+            <FaBriefcase color="#795548" size={22} />
             {profile.company}
           </Tooltip>
         </li>
       )}
       <li>
-        <FaUsers color='rgb(129, 195, 245)' size={22} />
+        <FaUsers color="rgb(129, 195, 245)" size={22} />
         {profile.followers.toLocaleString()} followers
       </li>
       <li>
-        <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+        <FaUserFriends color="rgb(64, 183, 95)" size={22} />
         {profile.following.toLocaleString()} following
       </li>
     </ul>
@@ -47,47 +62,65 @@ ProfileList.propTypes = {
   profile: PropTypes.object.isRequired,
 }
 
-export default class Results extends React.Component {
+export interface IPlayer {
+  score: number
+  profile: IProfile
+}
+
+export default class Results extends React.Component<
+  {
+    location: { search: string }
+  },
+  {
+    winner: null | IPlayer
+    loser: null | IPlayer
+    error: null | string
+    loading: boolean
+  }
+> {
   state = {
     winner: null,
     loser: null,
     error: null,
-    loading: true
+    loading: true,
   }
-  componentDidMount () {
-    const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
 
-    battle([ playerOne, playerTwo ])
-      .then((players) => {
+  componentDidMount() {
+    const { playerOne, playerTwo } = queryString.parse(
+      this.props.location.search
+    ) as { [id: string]: string }
+
+    battle([playerOne, playerTwo])
+      .then(players => {
         this.setState({
           winner: players[0],
           loser: players[1],
           error: null,
-          loading: false
+          loading: false,
         })
-      }).catch(({ message }) => {
+      })
+      .catch(({ message }) => {
         this.setState({
           error: message,
-          loading: false
+          loading: false,
         })
       })
   }
+
   render() {
     const { winner, loser, error, loading } = this.state
 
-    if (loading === true) {
-      return <Loading text='Battling' />
+    if (loading === true || !winner || !loser) {
+      return <Loading text="Battling" />
     }
 
     if (error) {
-      return (
-        <p className='center-text error'>{error}</p>
-      )
+      return <p className="center-text error">{error}</p>
     }
 
     return (
       <React.Fragment>
-        <div className='grid space-around container-sm'>
+        <div className="grid space-around container-sm">
           <Card
             header={winner.score === loser.score ? 'Tie' : 'Winner'}
             subheader={`Score: ${winner.score.toLocaleString()}`}
@@ -95,7 +128,7 @@ export default class Results extends React.Component {
             href={winner.profile.html_url}
             name={winner.profile.login}
           >
-            <ProfileList profile={winner.profile}/>
+            <ProfileList profile={winner.profile} />
           </Card>
           <Card
             header={winner.score === loser.score ? 'Tie' : 'Loser'}
@@ -104,13 +137,11 @@ export default class Results extends React.Component {
             name={loser.profile.login}
             href={loser.profile.html_url}
           >
-            <ProfileList profile={loser.profile}/>
+            <ProfileList profile={loser.profile} />
           </Card>
         </div>
-        <Link
-          to='/battle'
-          className='btn dark-btn btn-space'>
-            Reset
+        <Link to="/battle" className="btn dark-btn btn-space">
+          Reset
         </Link>
       </React.Fragment>
     )
