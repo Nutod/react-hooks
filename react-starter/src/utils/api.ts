11a008bbe7c50eb540c067c19d43412ebdc89d7a
Nutod <alukodotun@gmail.com>
2021-07-24 15:59:55 +0700

    Added new error function to api file

--- react-starter/src/utils/api.ts
+++ react-starter/src/utils/api.ts
@@ -1,4 +1,32 @@
-export default function fetchPopularRepos(language: string) {
+import { IRepo } from '../types/types'
+
+const id = 'YOUR_CLIENT_ID'
+const sec = 'YOUR_SECRET_ID'
+const params = `?client_id=${id}&client_secret=${sec}`
+
+// different functions to fetch data from the endpoints
+
+function getErrorMsg(message: string, username: string) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`
+  }
+
+  return message
+}
+
+export function getProfile(username: string) {
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then(response => response.json())
+    .then(profile => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username))
+      }
+
+      return profile
+    })
+}
+
+export function fetchPopularRepos(language: string) {
   const endpoint = window.encodeURI(
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`,
   )
@@ -10,6 +38,6 @@ export default function fetchPopularRepos(language: string) {
         throw new Error(data.message)
       }
 
-      return data.items
+      return data.items as IRepo[]
     })
 }
