1f367cb82056b12b4c3437de5719f73c84beeb70
Nutod <alukodotun@gmail.com>
2021-07-24 13:09:51 +0700

    Removed unused import from Card component

--- react-hooks-course/app/components/Card.tsx
+++ react-hooks-course/app/components/Card.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeConsumer } from '../contexts/theme'
 import { useSnapshot } from 'valtio'
 import { themeState } from '../store/store'
 import { ReactNode } from 'react'
