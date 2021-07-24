fb3421b60abf040385fde03d7d53e4e64097eeb0
Nutod <alukodotun@gmail.com>
2021-07-24 16:12:25 +0700

    Updated api file

--- react-starter/src/utils/api.ts
+++ react-starter/src/utils/api.ts
@@ -1,4 +1,4 @@
-import { IRepo } from '../types/types'
+import { IProfile, IRepo } from '../types/types'
 
 const id = 'YOUR_CLIENT_ID'
 const sec = 'YOUR_SECRET_ID'
@@ -22,8 +22,53 @@ export function getProfile(username: string) {
         throw new Error(getErrorMsg(profile.message, username))
       }
 
-      return profile
+      return profile as IProfile
     })
+}
+
+export function getRepos(username: string) {
+  return fetch(
+    `https://api.github.com/users/${username}/repos${params}&per_page=100`,
+  )
+    .then(response => response.json())
+    .then(repos => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username))
+      }
+
+      return repos as IRepo[]
+    })
+}
+
+function getStarCount(repos: IRepo[]) {
+  return repos.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0,
+  )
+}
+
+function calculateScore(followers: number, repos: IRepo[]) {
+  return followers * 3 + getStarCount(repos)
+}
+
+// get all the repos and profile
+function getUserData(player: string) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    }),
+  )
+}
+
+function sortPlayers(players: { score: number }[]) {
+  return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle(players: string[]) {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(
+    results => sortPlayers(results),
+  )
 }
 
 export function fetchPopularRepos(language: string) {
