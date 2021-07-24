c40988a58cf905c595832073411d176e599f3783
Nutod <alukodotun@gmail.com>
2021-07-24 13:05:54 +0700

    Updated theme state

--- react-hooks-course/app/store/store.ts
+++ react-hooks-course/app/store/store.ts
@@ -3,10 +3,10 @@
 // we need a theme state and a function to toggle it
 import { proxy } from 'valtio'
 
-export const state: { theme: 'light' | 'dark' } = proxy({
+export const themeState: { theme: 'light' | 'dark' } = proxy({
   theme: 'light',
 })
 
 export const toggleTheme = () => {
-  state.theme = state.theme === 'light' ? 'dark' : 'light'
+  themeState.theme = themeState.theme === 'light' ? 'dark' : 'light'
 }
