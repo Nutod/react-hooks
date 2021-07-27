bc11eabbddeb4aca9e0b88b0bee094753166eca6
Nutod <alukodotun@gmail.com>
2021-07-27 13:29:32 +0700

    Added new component to Popular

--- react-clone/src/components/Popular.tsx
+++ react-clone/src/components/Popular.tsx
@@ -1,9 +1,76 @@
 import React from 'react'
+import styled from 'styled-components'
+import { container } from '../styles/utils/container'
+
+const PopularWrapper = styled.div`
+  ${container}
+  margin-block-start: var(--space-500);
+
+  ul {
+    background: pink;
+    display: flex;
+    justify-content: center;
+    gap: var(--space-400);
+
+    li {
+      cursor: pointer;
+    }
+  }
+`
+
+type Language = 'All' | 'Javascript' | 'Python' | 'Ruby' | 'Java' | 'CSS'
+
+function SelectionNav({
+  selectedLanguage,
+  setSelectedLanguage,
+  languages,
+}: {
+  selectedLanguage: Language
+  setSelectedLanguage: (param: Language) => void
+  languages: Language[]
+}) {
+  return (
+    <ul>
+      {languages.map(language => (
+        <li
+          key={language}
+          onClick={() => setSelectedLanguage(language)}
+          style={{
+            borderBottom:
+              selectedLanguage === language
+                ? '2px solid var(--color-primary)'
+                : 0,
+          }}
+        >
+          {language}
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default function Popular() {
+  const languages: Language[] = [
+    'All',
+    'Javascript',
+    'Python',
+    'Ruby',
+    'Java',
+    'CSS',
+  ]
+  const [selectedLanguage, setSelectedLanguage] =
+    React.useState<Language>('All')
+
+  const getLanguageNavProps = () => ({
+    selectedLanguage,
+    setSelectedLanguage,
+    languages,
+  })
+
   return (
-    <div>
-      <h3>Popular Component</h3>
-    </div>
+    <PopularWrapper>
+      <SelectionNav {...getLanguageNavProps()} />
+      {/* Content */}
+    </PopularWrapper>
   )
 }
