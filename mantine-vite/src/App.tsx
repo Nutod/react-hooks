8d86ea842b835e4b1b9c384755d0462586a1b7e2
Nutod <alukodotun@gmail.com>
2021-07-27 17:14:25 +0700

    Moved files around

--- mantine-vite/src/App.tsx
+++ mantine-vite/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+
+// Config and then defaults
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
         </p>
