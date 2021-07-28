eb7a4a211861ca671b153bf0603e7b38d862b453
Nutod <alukodotun@gmail.com>
2021-07-28 13:21:57 +0700

    Added new assets

--- mantine-vite/src/components/Features.tsx
+++ mantine-vite/src/components/Features.tsx
@@ -4,11 +4,6 @@ export default function Features() {
   return (
     <div>
       <h3>Features</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolor
-        nulla dignissimos! At dicta laudantium sit, reprehenderit omnis nostrum
-        delectus.
-      </p>
     </div>
   )
 }
