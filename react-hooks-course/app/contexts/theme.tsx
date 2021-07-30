b9ab544c9cd15d329e9d1d9f9fe649c290299580
Nutod <alukodotun@gmail.com>
2021-07-30 15:57:04 +0700

    Moved files and cconfiguration around

--- react-hooks-course/app/contexts/theme.tsx
+++ react-hooks-course/app/contexts/theme.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-const { Consumer, Provider } = React.createContext()
+const { Consumer, Provider } = React.createContext({})
 
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+const ThemeContext = React.createContext(
+  {} as {
+    theme: 'light' | 'dark'
+    toggleTheme: () => void
+  }
+)
+
+export function ThemeContextProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme => (theme === 'dark' ? 'light' : 'dark'))
+  }, [])
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export function useThemeContext() {
+  const context = React.useContext(ThemeContext)
+
+  if (!context) throw new Error('ThemeContext is not defined')
+
+  return context
+}
