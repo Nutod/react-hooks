69c4098a80677f16479dd966e692502ce1746a45
Nutod <alukodotun@gmail.com>
2021-07-30 15:57:53 +0700

    Added new context value

--- react-hooks-course/app/app.tsx
+++ react-hooks-course/app/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.css'
-import { ThemeProvider } from './contexts/theme'
+import { useThemeContext } from './contexts/theme'
 import Nav from './components/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Loading from './components/Loading'
@@ -10,7 +10,7 @@ const Battle = React.lazy(() => import('./components/Battle'))
 const Results = React.lazy(() => import('./components/Results'))
 
 export default function App() {
-  const theme = 'light'
+  const { theme } = useThemeContext()
 
   return (
     <Router>
