9a29fdda026333d2e921e20538dda187315bc624
Nutod <alukodotun@gmail.com>
2021-07-31 14:21:42 +0700

    Updated api file

--- react-hooks-course-curriculum/app/components/Post.tsx
+++ react-hooks-course-curriculum/app/components/Post.tsx
@@ -14,6 +14,10 @@ export interface IPost {
   time: number
   text: string
   descendants: number
+  dead: boolean
+  deleted: boolean
+  type: 'comment' | 'story'
+  kids: string[]
 }
 
 export default function Post({ location }: { location: { search: string } }) {
