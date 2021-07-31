58b4f92fb969a263e09ecfa71cbc3083b24a701e
Nutod <alukodotun@gmail.com>
2021-07-31 13:47:26 +0700

    Added new ThemeContext api

--- react-hooks-course-curriculum/app/contexts/theme.ts
+++ react-hooks-course-curriculum/app/contexts/theme.ts
@@ -1,6 +1,23 @@
 import React from 'react'
+import constate from 'constate'
 
-const { Consumer, Provider } = React.createContext()
+const { Consumer, Provider } = React.createContext({})
 
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+function useTheme({
+  defaultTheme = 'light',
+}: {
+  defaultTheme: 'light' | 'dark'
+}) {
+  const [theme, setTheme] = React.useState(defaultTheme)
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
+  }, [])
+
+  return { theme, toggleTheme }
+}
+
+export const [ThemeContextProvider, useThemeContext] = constate(useTheme)
