f7046a6e8675fad4c8ef231caf98f324f7fe7c6b
Nutod <alukodotun@gmail.com>
2021-08-02 10:51:47 +0700

    Added new routes

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,44 +1,28 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import './App.css'
+import Battle from './components/Battle'
+import Nav from './components/Nav'
+import Popular from './components/Popular'
+import Results from './components/Results'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <Nav />
+
+      <Switch>
+        <Route path="/" exact>
+          <Popular />
+        </Route>
+        <Route path="/battle">
+          <Battle />
+        </Route>
+        <Route path="/battle/results">
+          <Results />
+        </Route>
+      </Switch>
+    </>
   )
 }
 
