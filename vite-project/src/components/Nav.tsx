f27df924a5eefe21cfe919245296096183a76df4
Nutod <alukodotun@gmail.com>
2021-08-02 10:55:14 +0700

    Added navigation base styles

--- vite-project/src/components/Nav.tsx
+++ vite-project/src/components/Nav.tsx
@@ -1,5 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavWrapper = styled.nav`
+  padding-block: var(--space-100);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ul {
+    display: flex;
+    gap: var(--space-100);
+  }
+`
 
 export default function Nav() {
-  return <div>Navigation</div>
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/">Popular</Link>
+        </li>
+        <li>
+          <Link to="/battle">Battle</Link>
+        </li>
+      </ul>
+      <div>
+        <button>Light</button>
+      </div>
+    </NavWrapper>
+  )
 }
