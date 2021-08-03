b051cc3cf15ca91bb05ce206d073c9065f1f7b20
Nutod <alukodotun@gmail.com>
2021-08-03 16:50:37 +0700

    Updated Button component

--- stitches-dev/src/components/Button.tsx
+++ stitches-dev/src/components/Button.tsx
@@ -1,7 +1,19 @@
 import React, { ReactNode } from 'react'
 import { styled } from '../styles/stitches.config'
 
-const ButtonWrapper = styled('button', {})
+const ButtonWrapper = styled('button', {
+  color: 'white',
+  background: '$color-primary',
+  padding: '10px 15px',
+  fontFamily: '$display',
+  border: '0',
+
+  '& :hover': {
+    background: '$color-primary-shade',
+    boxShadow: '$shadow-md',
+    cursor: 'pointer',
+  },
+})
 
 export default function Button({ children }: { children: ReactNode }) {
   return <ButtonWrapper>{children}</ButtonWrapper>
