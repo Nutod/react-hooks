ce4fb80416e5e1587262e02497a33c569eb2ca3b
Nutod <alukodotun@gmail.com>
2021-08-03 17:25:26 +0700

    Updated Footer component

--- stitches-dev/src/components/Footer.tsx
+++ stitches-dev/src/components/Footer.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import { styled } from '../styles/stitches.config'
+
+const FooterWrapper = styled('footer', {
+  background: '$color-secondary',
+  padding: '$400',
+  color: 'white',
+
+  '& ul': {
+    listStyle: 'none',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(10rem, 1fr))',
+    gap: '$200',
+  },
+})
 
 export default function Footer() {
-  return <div>Footer...</div>
+  return (
+    <FooterWrapper>
+      <ul>
+        <li>Footer Link 1</li>
+        <li>Footer Link 2</li>
+        <li>Footer Link 3</li>
+        <li>Footer Link 4</li>
+        <li>Footer Link 5</li>
+        <li>Footer Link 6</li>
+      </ul>
+    </FooterWrapper>
+  )
 }
