b25aa7e50ed18907ba4e3b9c50f1178066418e64
Nutod <alukodotun@gmail.com>
2021-08-03 16:27:07 +0700

    Updated config file

--- stitches-dev/src/components/Sidebar.tsx
+++ stitches-dev/src/components/Sidebar.tsx
@@ -1,5 +1,25 @@
 import React from 'react'
+import { styled } from '../styles/stitches.config'
+
+const SidebarWrapper = styled('aside', {})
 
 export default function Sidebar() {
-  return <div>Sidebar...</div>
+  return (
+    <aside>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4 6h16M4 12h8m-8 6h16"
+        />
+      </svg>
+    </aside>
+  )
 }
--- stitches-dev/src/styles/stitches.config.ts
+++ stitches-dev/src/styles/stitches.config.ts
@@ -116,4 +116,49 @@ export const { styled, css, global, keyframes, getCssString, theme } =
       zIndices: {},
       transitions: {},
     },
+    utils: {
+      // Abbreviated margin properties
+      m: config => value => ({
+        marginTop: value,
+        marginBottom: value,
+        marginLeft: value,
+        marginRight: value,
+      }),
+      mt: config => value => ({
+        marginTop: value,
+      }),
+      mr: config => value => ({
+        marginRight: value,
+      }),
+      mb: config => value => ({
+        marginBottom: value,
+      }),
+      ml: config => value => ({
+        marginLeft: value,
+      }),
+      mx: config => value => ({
+        marginLeft: value,
+        marginRight: value,
+      }),
+      my: config => value => ({
+        marginTop: value,
+        marginBottom: value,
+      }),
+
+      // A property for applying width/height together
+      size: config => value => ({
+        width: value,
+        height: value,
+      }),
+
+      // A property to apply linear gradient
+      linearGradient: config => value => ({
+        backgroundImage: `linear-gradient(${value})`,
+      }),
+
+      // An abbreviated property for border-radius
+      br: config => value => ({
+        borderRadius: value,
+      }),
+    },
   })
