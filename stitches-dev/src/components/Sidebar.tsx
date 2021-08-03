36e4bbf4e54d74ec7b1038a8f068022221f312a8
Nutod <alukodotun@gmail.com>
2021-08-03 16:30:58 +0700

    Added new styles to Sidebar component

--- stitches-dev/src/components/Sidebar.tsx
+++ stitches-dev/src/components/Sidebar.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { styled } from '../styles/stitches.config'
 
-const SidebarWrapper = styled('aside', {})
+const SidebarWrapper = styled('aside', {
+  background: '$color-primary',
+  display: 'flex',
+  alignItems: 'center',
+  padding: '$100',
+
+  '& svg': {
+    blockSize: '$500',
+    color: 'white',
+  },
+})
 
 export default function Sidebar() {
   return (
-    <aside>
+    <SidebarWrapper>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-6 w-6"
@@ -20,6 +30,6 @@ export default function Sidebar() {
           d="M4 6h16M4 12h8m-8 6h16"
         />
       </svg>
-    </aside>
+    </SidebarWrapper>
   )
 }
