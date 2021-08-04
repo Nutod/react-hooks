48be3a7ffcf401b3efa64fba9946d25c4dbc237a
Nutod <alukodotun@gmail.com>
2021-08-04 10:05:06 +0700

    Updated Stories component

--- stitches-dev/src/components/Stories.tsx
+++ stitches-dev/src/components/Stories.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import { styled } from '../styles/stitches.config'
+import StoryOne from '../assets/story-1.jpeg'
+import StoryTwo from '../assets/story-2.jpeg'
+
+const StoriesPictures = styled('div', {
+  background: 'papayawhip',
+  padding: '$400',
+
+  display: 'grid',
+  gridTemplateColumns: 'repeat(6, 1fr)',
+  gridTemplateRows: 'repeat(6, 10vw)',
+
+  '& figure': {
+    objectFit: 'cover',
+  },
+
+  '& img': {
+    width: '100%',
+    height: '100%',
+  },
+})
+
+const StoriesContent = styled('div', {
+  background: 'pink',
+  padding: '$400',
+})
 
 export default function Stories() {
   return (
     <>
-      <div>Stories Picture</div>
-      <div>Stories Content</div>
+      <StoriesPictures>
+        <figure>
+          <img className="main_image" src={StoryOne} alt="" />
+          {/* <img className="sub_image" src={StoryTwo} alt="" /> */}
+        </figure>
+      </StoriesPictures>
+      <StoriesContent>Stories Content</StoriesContent>
     </>
   )
 }
