10f2e381cae1e0770b3dc8bd837e56ce5a243b8a
Nutod <alukodotun@gmail.com>
2021-08-05 18:13:35 +0700

    Added base styling to realtors

--- mantine-vite/src/components/Realtors.tsx
+++ mantine-vite/src/components/Realtors.tsx
@@ -1,5 +1,28 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
+import { theming } from '@mantine/core'
+import { theme as extendedTheme } from '../config/extendedTheme'
+
+const useStyles = createUseStyles(
+  theme => ({
+    wrapper: {
+      background: extendedTheme.colors['color-secondary'],
+      color: theme.white,
+    },
+    container: {
+      maxWidth: '70rem',
+      marginInline: 'auto',
+    },
+  }),
+  { theming }
+)
 
 export default function Realtors() {
-  return <div>Realtors...</div>
+  const styles = useStyles()
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}></div>
+    </div>
+  )
 }
