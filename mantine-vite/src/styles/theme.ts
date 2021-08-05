6c7339d9273f97355aff87c4168baa312d6a0bd4
Nutod <alukodotun@gmail.com>
2021-08-05 16:44:43 +0700

    Updated Mantine theme

---
+++ mantine-vite/src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colorScheme: 'light',
+  headings: {
+    fontFamily: 'Josefin Sans, sans-serif',
+    fontWeight: 400,
+    sizes: {
+      h1: { fontSize: '1.802rem', lineHeight: 1.3 },
+      h2: { fontSize: '1.602rem', lineHeight: 1.3 },
+      h3: { fontSize: '1.424rem', lineHeight: 1.3 },
+      h4: { fontSize: '1.266rem', lineHeight: 1.3 },
+      h5: { fontSize: '1.125rem', lineHeight: 1.3 },
+      h6: { fontSize: '1.802rem', lineHeight: 1.3 },
+    },
+  },
+}
