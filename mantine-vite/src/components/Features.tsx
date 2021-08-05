5524517105ec36e4cf70d870b30055f10d75323c
Nutod <alukodotun@gmail.com>
2021-08-05 17:58:30 +0700

    Updated Features component styles

--- mantine-vite/src/components/Features.tsx
+++ mantine-vite/src/components/Features.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
 import { Text, theming, Title } from '@mantine/core'
+import { theme as extendedTheme } from '../config/extendedTheme'
 
 const useStyles = createUseStyles(
   theme => ({
     container: {
       maxWidth: '70rem',
       marginInline: 'auto',
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(18rem, 1fr))',
+      gap: theme.spacing.md,
+      paddingInline: theme.spacing.lg,
+      paddingBlock: theme.spacing.xl,
     },
-    feature: {},
+    feature: {
+      '& h4': {
+        color: extendedTheme.colors['color-primary'],
+      },
+    },
   }),
   { theming }
 )
@@ -19,42 +29,42 @@ export default function Features() {
   return (
     <div className={styles.container}>
       <div className={styles.feature}>
-        <Title>World's best luxury homes</Title>
+        <Title order={4}>World's best luxury homes</Title>
         <Text component="p">
           Lorem ipsum dolor sit amet reprehenderit deleniti quam possimus
           architecto inventore veritatis.
         </Text>
       </div>
       <div className={styles.feature}>
-        <Title>Only the best properties</Title>
+        <Title order={4}>Only the best properties</Title>
         <Text component="p">
           Cumque consequuntur veniam dolorum ratione sint illo placeat numquam
           ullam odio itaque ab eos, debitis quidem culpa!
         </Text>
       </div>
       <div className={styles.feature}>
-        <Title>All homes in top location</Title>
+        <Title order={4}>All homes in top location</Title>
         <Text component="p">
           Quaerat consequuntur obcaecati nihil et reprehenderit deleniti quam
           veritatis.
         </Text>
       </div>
       <div className={styles.feature}>
-        <Title>New home in one week</Title>
+        <Title order={4}>New home in one week</Title>
         <Text component="p">
           Architecto praesentium sed deserunt delectus consectetur quod fugit
           veniam.
         </Text>
       </div>
       <div className={styles.feature}>
-        <Title>Top 1% realtors</Title>
+        <Title order={4}>Top 1% realtors</Title>
         <Text component="p">
           Explicabo atque unde dolorem tempora facilis fugit, cum minus ad sint
           tempore minima sed libero aperiam magnam aliquid.
         </Text>
       </div>
       <div className={styles.feature}>
-        <Title>World's best luxury homes</Title>
+        <Title order={4}>World's best luxury homes</Title>
         <Text component="p">
           Lorem ipsum dolor sit amet reprehenderit deleniti quam possimus
           architecto inventore veritatis.
--- mantine-vite/src/pages/Home.tsx
+++ mantine-vite/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Features from '../components/Features'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
     <>
       <Sidebar />
+      <Features />
     </>
   )
 }
