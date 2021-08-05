aaefe94da0dadc0419d889f7c44f3733c918bbfa
Nutod <alukodotun@gmail.com>
2021-08-05 19:16:54 +0700

    Updated Homes component styling

--- mantine-vite/src/components/Homes.tsx
+++ mantine-vite/src/components/Homes.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
-import { Button, theming, Title } from '@mantine/core'
+import { Button, Text, theming, Title } from '@mantine/core'
 import { theme as extendedTheme } from '../config/extendedTheme'
 import HouseOne from '../assets/house-1.jpeg'
 
@@ -14,18 +14,46 @@ const useStyles = createUseStyles(
         blockSize: '2rem',
         color: extendedTheme.colors['color-primary'],
       },
+
+      '& h5': {
+        background: extendedTheme.colors['color-secondary'],
+        color: theme.white,
+        padding: theme.spacing.sm,
+        width: '70%',
+        margin: '0 auto',
+        transform: 'translateY(-50%)',
+        textAlign: 'center',
+      },
     },
     container: {
       maxWidth: '70rem',
       marginInline: 'auto',
     },
     homes: {},
-    home: {},
+    home: {
+      position: 'relative',
+      background: theme.white,
+
+      '& > * + *': {
+        marginBlockStart: theme.spacing.md,
+      },
+    },
+    icon: {
+      position: 'absolute',
+      right: '2px',
+    },
+    description: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(10rem, 1fr))',
+      gap: theme.spacing.md,
+      textAlign: 'center',
+    },
     button: {
       fontFamily: extendedTheme.fonts.display,
       background: extendedTheme.colors['color-primary'],
       textShadow: 'none',
       boxShadow: extendedTheme.shadows['shadow-md'],
+      width: '100%',
     },
   }),
   { theming }
@@ -41,7 +69,7 @@ export default function Homes() {
           <div className={styles.home}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className={styles.icon}
               viewBox="0 0 20 20"
               fill="currentColor"
             >
@@ -53,6 +81,68 @@ export default function Homes() {
             </svg>
             <img src={HouseOne} alt="House One" />
             <Title order={5}>Beautiful Family House</Title>
+            <div className={styles.description}>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <Text>USA</Text>
+              </div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                </svg>
+                <Text>5 Rooms</Text>
+              </div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4"
+                  />
+                </svg>
+                <Text>325m</Text>
+              </div>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <Text>$1,200,000</Text>
+              </div>
+            </div>
             <Button className={styles.button}>Contact Realtor</Button>
           </div>
         </div>
