af6862a2ba1f85830c3423ed4c20e5209804354a
Nutod <alukodotun@gmail.com>
2021-08-05 16:32:37 +0700

    Initialised new project

--- mantine-vite/src/App.tsx
+++ mantine-vite/src/App.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import styled from 'styled-components'
-import Sidebar from './components/Sidebar'
-import Hero from './components/Hero'
-import Realtors from './components/Realtors'
-import Features from './components/Features'
-
-const Container = styled.div``
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Container>
-      <Sidebar />
-      <Hero />
-      <Realtors />
-      <Features />
-    </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
   )
 }
 
--- mantine-vite/src/main.tsx
+++ mantine-vite/src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { MantineProvider, GlobalStyles } from '@mantine/core'
+import './index.css'
 import App from './App'
-import { theme } from './styles/theme'
-import { GlobalStyledElements } from './styles/global'
 
 ReactDOM.render(
   <React.StrictMode>
-    <MantineProvider theme={theme}>
-      <GlobalStyledElements />
-      <GlobalStyles />
-      <App />
-    </MantineProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
