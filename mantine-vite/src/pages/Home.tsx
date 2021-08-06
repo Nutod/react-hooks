01c632332018b56db1f9d24bbf0cd037f997547a
Nutod <alukodotun@gmail.com>
2021-08-06 11:12:53 +0700

    Added new media query component to home page

--- mantine-vite/src/pages/Home.tsx
+++ mantine-vite/src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
+import { theming } from '@mantine/core'
 import Features from '../components/Features'
 import Footer from '../components/Footer'
 import Homes from '../components/Homes'
@@ -6,15 +8,32 @@ import Realtors from '../components/Realtors'
 import Sidebar from '../components/Sidebar'
 import Story from '../components/Story'
 
+const useStyles = createUseStyles(
+  theme => ({
+    container: {
+      background: 'orangered',
+
+      '@media screen and (min-width: 1024px)': {
+        display: 'grid',
+        gridTemplateColumns:
+          'minmax(6rem, 1fr) repeat(8, minmax(min-content, 14rem)) minmax(6rem, 1fr)',
+      },
+    },
+  }),
+  { theming }
+)
+
 export default function Home() {
+  const styles = useStyles()
+
   return (
-    <>
+    <div className={styles.container}>
       <Sidebar />
       <Realtors />
       <Features />
       <Homes />
       <Story />
       <Footer />
-    </>
+    </div>
   )
 }
