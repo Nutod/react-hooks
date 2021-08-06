41d879ea6536ec798f53af69f44c5c4c563ce7f5
Nutod <alukodotun@gmail.com>
2021-08-06 14:08:37 +0700

    Updated SelectionNav component

--- react-github/src/components/Popular.tsx
+++ react-github/src/components/Popular.tsx
@@ -1,9 +1,81 @@
 import React from 'react'
-import { Title, Text } from '@mantine/core'
+import { Title, Text, Container, Center } from '@mantine/core'
+import { createUseStyles } from 'react-jss'
+import { theming } from '@mantine/core'
+import { extendedTheme } from '../config/extendedTheme'
+import { CSSProperties } from 'react'
+
+const useStyles = createUseStyles(
+  theme => ({
+    container: {
+      background: 'pink',
+    },
+    unorderedList: {
+      listStyle: 'none',
+      display: 'flex',
+      gap: theme.spacing.sm,
+
+      '& li': {
+        cursor: 'pointer',
+      },
+    },
+  }),
+  { theming }
+)
+
+type Language = 'All' | 'Javascript' | 'Python' | 'Ruby' | 'Java'
+
+function SelectionNav({
+  selected,
+  setSelected,
+  styles,
+  languages,
+}: {
+  selected: Language
+  setSelected: (param: Language) => void
+  styles: string
+  languages: Language[]
+}) {
+  return (
+    <ul className={styles}>
+      {languages.map(language => (
+        <li
+          key={language}
+          onClick={() => setSelected(language)}
+          style={{
+            borderBottom:
+              selected === language
+                ? `2px solid ${extendedTheme.colors['color-primary']}`
+                : 'none',
+          }}
+        >
+          {language}
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 export default function Popular() {
+  const styles = useStyles()
+
+  const languages: Language[] = ['All', 'Javascript', 'Python', 'Ruby', 'Java']
+  const [selected, setSelected] = React.useState<Language>('All')
+
+  const getSelectionNavProps = () => ({
+    selected,
+    setSelected,
+    styles: styles['unorderedList'],
+    languages,
+  })
+
   return (
-    <>
+    <Container size="md" className={styles.container}>
+      <Center>
+        <SelectionNav {...getSelectionNavProps()} />
+      </Center>
+      {/* Language Nav */}
+      {/* Data Content */}
       <Title order={1}>Level Heading</Title>
       <Title order={2}>Level Heading</Title>
       <Title order={3}>Level Heading</Title>
@@ -15,6 +87,6 @@ export default function Popular() {
         quasi incidunt nemo laudantium dolorum harum aliquam aliquid fugiat
         quia?
       </Text>
-    </>
+    </Container>
   )
 }
---
+++ react-github/src/utils/api.ts
@@ -0,0 +1,84 @@
+const id = 'YOUR_CLIENT_ID'
+const sec = 'YOUR_SECRET_ID'
+const params = `?client_id=${id}&client_secret=${sec}`
+
+function getErrorMsg(message, username) {
+  if (message === 'Not Found') {
+    return `${username} doesn't exist`
+  }
+
+  return message
+}
+
+export function getProfile(username) {
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then(response => response.json())
+    .then(profile => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username))
+      }
+
+      return profile
+    })
+}
+
+export function getRepos(username) {
+  return fetch(
+    `https://api.github.com/users/${username}/repos${params}&per_page=100`
+  )
+    .then(response => response.json())
+    .then(repos => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username))
+      }
+
+      return repos
+    })
+}
+
+function getStarCount(repos) {
+  return repos.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  )
+}
+
+function calculateScore(followers: number, repos) {
+  return followers * 3 + getStarCount(repos)
+}
+
+// get all the repos and profile
+function getUserData(player: string) {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos),
+    })
+  )
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle(players: string[]) {
+  return Promise.all([getUserData(players[0]), getUserData(players[1])]).then(
+    results => sortPlayers(results)
+  )
+}
+
+export function fetchPopularRepos(language: string) {
+  const endpoint = window.encodeURI(
+    `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+  )
+
+  return fetch(endpoint)
+    .then(res => res.json())
+    .then(data => {
+      if (!data.items) {
+        throw new Error(data.message)
+      }
+
+      return data.items
+    })
+}
