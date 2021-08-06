553fb252b820921c8ffd5d10a5b0b6239c990b3d
Nutod <alukodotun@gmail.com>
2021-08-06 10:49:21 +0700

    Added new UI logic to Story component

--- mantine-vite/src/components/Story.tsx
+++ mantine-vite/src/components/Story.tsx
@@ -1,5 +1,28 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
+import { theming } from '@mantine/core'
+import ImageOne from '../assets/story-1.jpeg'
+import ImageTwo from '../assets/story-2.jpeg'
+
+const useStyles = createUseStyles(
+  theme => ({
+    pictures: {},
+    content: {},
+  }),
+  { theming }
+)
 
 export default function Story() {
-  return <div>Story...</div>
+  const styles = useStyles()
+
+  return (
+    <>
+      <div className={styles.pictures}>
+        <figure>
+          <img src={ImageOne} alt="Image One" />
+        </figure>
+      </div>
+      <div className={styles.content}>Content</div>
+    </>
+  )
 }
