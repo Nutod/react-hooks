68a1d6efcb6856083dbc997f331316c1964631bb
Nutod <alukodotun@gmail.com>
2021-08-06 11:15:47 +0700

    Added new media query to Homes component

--- mantine-vite/src/components/Homes.tsx
+++ mantine-vite/src/components/Homes.tsx
@@ -30,6 +30,10 @@ const useStyles = createUseStyles(
       '& img': {
         margin: 0,
       },
+
+      '@media screen and (min-width: 1024px)': {
+        gridColumn: '1 / -1',
+      },
     },
     container: {
       maxWidth: '70rem',
