9d7390fb35390504d6607091c77b36e89fade065
Nutod <alukodotun@gmail.com>
2021-08-07 12:15:26 +0700

    Updated Loading component

--- react-hooks-course/app/components/Loading.tsx
+++ react-hooks-course/app/components/Loading.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { CSSProperties } from 'react'
 
 const styles = {
   content: {
@@ -9,30 +10,24 @@ const styles = {
     right: '0',
     marginTop: '20px',
     textAlign: 'center',
-  }
+  } as CSSProperties,
 }
 
-export default class Loading extends React.Component {
-  state = { content: this.props.text }
-  componentDidMount () {
-    const { speed, text } = this.props
+export default function Loading({ text = 'Loading', speed = 300 }) {
+  const [content, setContent] = React.useState(text)
+  const intervalId = React.useRef<number>()
 
-    this.interval = window.setInterval(() => {
-      this.state.content === text + '...'
-        ? this.setState({ content: text })
-        : this.setState(({ content }) => ({ content: content + '.' }))
+  React.useEffect(() => {
+    intervalId.current = window.setInterval(() => {
+      content === text + '...'
+        ? setContent(text)
+        : setContent(content => content + '.')
     }, speed)
-  }
-  componentWillUnmount () {
-    window.clearInterval(this.interval)
-  }
-  render() {
-    return (
-      <p style={styles.content}>
-        {this.state.content}
-      </p>
-    )
-  }
+
+    return () => window.clearInterval(intervalId.current)
+  }, [])
+
+  return <p style={styles.content}>{content}</p>
 }
 
 Loading.propTypes = {
@@ -42,5 +37,5 @@ Loading.propTypes = {
 
 Loading.defaultProps = {
   text: 'Loading',
-  speed: 300
+  speed: 300,
 }
