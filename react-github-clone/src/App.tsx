b8fabfa16dda8a97f1365fd4f71b439a8edf5e62
Nutod <alukodotun@gmail.com>
2021-08-13 15:56:41 +0700

    Added new components

--- react-github-clone/src/App.tsx
+++ react-github-clone/src/App.tsx
@@ -1,78 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Popular from './components/Popular'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <button
-            className="surface-1"
-            type="button"
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <button
-            className="surface-2"
-            type="button"
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <button
-            className="surface-3"
-            type="button"
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          <button
-            className="surface-4"
-            type="button"
-            onClick={() => setCount(count => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <input type="text" />
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    
+    <Routes>
+      <Route path="/" element={<Popular />} />
+    </Routes>
   )
 }
 
