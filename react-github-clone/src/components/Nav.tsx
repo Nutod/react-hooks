30702e677be4ae09b51b5ab75e152e1f76f13be2
Nutod <alukodotun@gmail.com>
2021-08-13 16:05:57 +0700

    Updated Nav component styling

--- react-github-clone/src/components/Nav.tsx
+++ react-github-clone/src/components/Nav.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const NavWrapper = styled.nav`
-  background-color: pink;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .links {
+    display: flex;
+    gap: var(--space-100);
+  }
+
+  form {
+    display: flex;
+    gap: var(--space-100);
+  }
 `
 
 export default function Nav() {
-  return <NavWrapper>Navigation...</NavWrapper>
+  return (
+    <NavWrapper>
+      <div className="links">
+        <Link to="/">Popular</Link>
+        <Link to="battle">Battle</Link>
+      </div>
+      <form>
+        <div>
+          <input type="radio" id="light" name="theme" checked />
+          <label htmlFor="light">Light</label>
+        </div>
+        <div>
+          <input type="radio" id="dark" name="theme" />
+          <label htmlFor="dark">Dark</label>
+        </div>
+      </form>
+    </NavWrapper>
+  )
 }
