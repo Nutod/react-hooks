d9e5cfd4a2d8080fff81064f611d8a8608c278a1
Nutod <alukodotun@gmail.com>
2021-08-20 11:41:53 +0700

    Updated base routers

--- react-hook-course/src/App.tsx
+++ react-hook-course/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import '@geist-ui/style'
 import Nav from './components/Nav'
+import Popular from './components/Popular'
 
 function App() {
   return (
     <>
       <Nav />
+
+      <Routes>
+        <Route path="/" element={<Popular />} />
+      </Routes>
     </>
   )
 }
