0be1ddd17a595595be3b3a4c2ec70ce6d65de019
Nutod <alukodotun@gmail.com>
2021-08-20 11:49:01 +0700

    Added ui to Nav component

--- react-hook-course/src/App.tsx
+++ react-hook-course/src/App.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import '@geist-ui/style'
 import Nav from './components/Nav'
 import Popular from './components/Popular'
+import Battle from './components/Battle'
+import Result from './components/Result'
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Popular />} />
+        <Route path="battle" element={<Battle />} />
+        <Route path="battle/result" element={<Result />} />
+        {/* Not Found Here? */}
       </Routes>
     </>
   )
--- react-hook-course/src/components/Nav.tsx
+++ react-hook-course/src/components/Nav.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
-  return <div>Nav...</div>
+  return (
+    <nav
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <ul style={{ display: 'flex', gap: 'var(--space-400)' }}>
+        <li>
+          <Link to="/">Popular</Link>
+        </li>
+        <li>
+          <Link to="battle">Battle</Link>
+        </li>
+      </ul>
+      <div>
+        <button className="zi-btn mini shadow">Light</button>
+      </div>
+    </nav>
+  )
 }
