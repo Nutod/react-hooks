b508dfc88babcba91f7cb2fe56447757f89bd7d5
Nutod <alukodotun@gmail.com>
2021-08-28 14:55:02 +0700

    Added annotations to Hover component

--- react-hooks-course/app/components/Hover.tsx
+++ react-hooks-course/app/components/Hover.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-export default class Hover extends React.Component {
+export default class Hover extends React.Component<{
+  children: (param: boolean) => ReactNode
+}> {
   state = { hovering: false }
   mouseOver = () => this.setState({ hovering: true })
   mouseOut = () => this.setState({ hovering: false })
-  render () {
+  render() {
     return (
       <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
         {this.props.children(this.state.hovering)}
