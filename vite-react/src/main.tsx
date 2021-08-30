77cb73ca1d76a9f7da9f7af1ece20e3337ca772a
Nutod <alukodotun@gmail.com>
2021-08-30 15:05:55 +0700

    Added new components

--- vite-react/src/main.tsx
+++ vite-react/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { GeistProvider, CssBaseline } from '@geist-ui/react'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GeistProvider>
+      <CssBaseline />
+      <App />
+    </GeistProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
