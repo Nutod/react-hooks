d2d2cdaa99dac30e325feff4ae7370e4431e03e7
Nutod <alukodotun@gmail.com>
2021-08-30 15:10:09 +0700

    Updated components

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,11 +1,18 @@
-import { Text } from '@geist-ui/react'
 import React from 'react'
+import { Text } from '@geist-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import Popular from './components/Popular'
 
 function App() {
   return (
     <div>
       <Text h1>Navigation</Text>
       <h2>Popular component</h2>
+      <main>
+        <Routes>
+          <Route path="/" element={<Popular />} />
+        </Routes>
+      </main>
     </div>
   )
 }
---
+++ vite-react/src/components/Popular.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useDocumentTitle } from '@mantine/hooks'
+
+export default function Popular() {
+  useDocumentTitle('Github Popular Repos')
+
+  return <div>Popular...</div>
+}
