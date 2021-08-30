ad7c3f14a861c081975613bc306c9ffe361bcc8c
Nutod <alukodotun@gmail.com>
2021-08-30 15:29:08 +0700

    Updated main component

--- vite-react/src/main.tsx
+++ vite-react/src/main.tsx
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom'
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeContextProvider, useThemeContext } from './context/theme'
 
-ReactDOM.render(
-  <React.StrictMode>
+function Main() {
+  const { theme } = useThemeContext()
+
+  return (
     <Router>
-      <GeistProvider>
+      <GeistProvider themeType="light">
         <CssBaseline />
         <App />
       </GeistProvider>
     </Router>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeContextProvider defaultTheme="dark">
+      <Main />
+    </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
