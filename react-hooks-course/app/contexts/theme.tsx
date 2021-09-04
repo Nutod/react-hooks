f9f261ea983442fdbeb89472cae6e0ca0582366b
Nutod <alukodotun@gmail.com>
2021-09-04 13:09:59 +0700

    Added new theme context api

--- react-hooks-course/app/contexts/theme.tsx
+++ react-hooks-course/app/contexts/theme.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-const { Consumer, Provider } = React.createContext()
+const { Consumer, Provider } = React.createContext({})
 
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+function useMountedCallback<T>(toggleCb: () => () => void, options: T[]) {
+  return React.useCallback(toggleCb(), options)
+}
+
+const ThemeContext = React.createContext(
+  {} as {
+    theme: 'light' | 'dark'
+    toggleTheme: () => void
+  }
+)
+
+export function ThemeContextProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
+
+  function toggleCb() {
+    return () => {
+      setTheme(theme => (theme === 'dark' ? 'light' : 'dark'))
+    }
+  }
+
+  const toggleTheme = useMountedCallback(toggleCb, [])
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
