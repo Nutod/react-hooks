44a8063646017c4af1b9c6fc366352633177f0af
Nutod <alukodotun@gmail.com>
2021-09-06 14:32:26 +0700

    Added new theme context api

--- react-hooks-course-curriculum/app/contexts/theme.ts
+++ react-hooks-course-curriculum/app/contexts/theme.ts
@@ -1,6 +1,10 @@
 import React from 'react'
+import constate from 'constate'
+import { useTheme } from '../hooks/useTheme'
 
-const { Consumer, Provider } = React.createContext()
+const { Consumer, Provider } = React.createContext({})
 
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+export const [ThemeContextProvider, useThemeContext] = constate(useTheme)
