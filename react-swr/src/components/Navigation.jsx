d56b736cc87ef1c5f69988d4a9e720522fd9a5da
Nutod <alukodotun@gmail.com>
2021-09-06 23:01:13 +0700

    Added new nav component

---
+++ react-swr/src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>Users</li>
+        <li>Profile</li>
+      </ul>
+    </nav>
+  )
+}
