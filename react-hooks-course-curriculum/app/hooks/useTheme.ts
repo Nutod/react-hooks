c6257b64c8dfcc993e5f8a2114ae0af84061aefd
Nutod <alukodotun@gmail.com>
2021-09-06 14:31:05 +0700

    Added new custom hook

--- react-hooks-course-curriculum/app/hooks/useTheme.ts
+++ react-hooks-course-curriculum/app/hooks/useTheme.ts
@@ -1,1 +1,11 @@
 import React from 'react'
+
+export function useTheme({ defaultTheme }: { defaultTheme: 'light' | 'dark' }) {
+  const [theme, setTheme] = React.useState(defaultTheme)
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme => (theme === 'dark' ? 'light' : 'dark'))
+  }, [])
+
+  return { theme, toggleTheme }
+}
