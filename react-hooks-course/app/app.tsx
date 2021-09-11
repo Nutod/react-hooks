6d08535c9a3dedbae324b71a3344fbe5f505f513
Nutod <alukodotun@gmail.com>
2021-09-11 10:42:27 +0700

    Added new context api to app component

--- react-hooks-course/app/app.tsx
+++ react-hooks-course/app/app.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
-import { ThemeProvider } from './contexts/theme'
+import { useThemeContext } from './contexts/theme'
 import Nav from './components/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Loading from './components/Loading'
@@ -10,35 +9,25 @@ const Popular = React.lazy(() => import('./components/Popular'))
 const Battle = React.lazy(() => import('./components/Battle'))
 const Results = React.lazy(() => import('./components/Results'))
 
-export default class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light',
-      }))
-    },
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className="container">
-              <Nav />
+export default function App() {
+  const { theme } = useThemeContext()
 
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route exact path="/" component={Popular} />
-                  <Route exact path="/battle" component={Battle} />
-                  <Route path="/battle/results" component={Results} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
-    )
-  }
+  return (
+    <Router>
+      <div className={theme}>
+        <div className="container">
+          <Nav />
+
+          <React.Suspense fallback={<Loading />}>
+            <Switch>
+              <Route exact path="/" component={Popular} />
+              <Route exact path="/battle" component={Battle} />
+              <Route path="/battle/results" component={Results} />
+              <Route render={() => <h1>404</h1>} />
+            </Switch>
+          </React.Suspense>
+        </div>
+      </div>
+    </Router>
+  )
 }
