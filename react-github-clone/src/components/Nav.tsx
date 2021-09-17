13f001bbd09239bc451dbede47ab1218bf1fa009
Nutod <alukodotun@gmail.com>
2021-09-17 13:16:23 +0100

    Added new styling to Navs component

--- react-github-clone/src/components/Nav.tsx
+++ react-github-clone/src/components/Nav.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
+import { css } from 'linaria'
+import { Link } from 'react-router-dom'
+
+const classes = {
+  ul: css`
+    display: flex;
+    gap: 1rem;
+  `,
+  li: css`
+    &::before {
+      display: none;
+    }
+  `,
+}
 
 export default function Nav() {
-  return <nav>Navigation...</nav>
+  return (
+    <nav>
+      <ul className={classes.ul}>
+        <li className={classes.li}>
+          <Link to="/">Popular</Link>
+        </li>
+        <li className={classes.li}>
+          <Link to="battle">Battle</Link>
+        </li>
+      </ul>
+      <a>Light</a>
+    </nav>
+  )
 }
