6c09458b9aae70f8b92f0a218eb9e18abc023b7f
Nutod <alukodotun@gmail.com>
2021-09-18 10:33:01 +0100

    Added new types

---
+++ react-hooks-course/app/types/index.ts
@@ -0,0 +1,16 @@
+export type Language = 'All' | 'JavaScript' | 'Ruby' | 'Java' | 'CSS' | 'Python'
+
+export interface IOwner {
+  login: string
+  avatar_url: string
+  html_url: string
+}
+
+export interface IRepo {
+  name: string
+  owner: IOwner
+  html_url: string
+  stargazers_count: number
+  forks: number
+  open_issues: number
+}
