01caedf1b9f92fcb349a2556c5a08a307338989d
Nutod <alukodotun@gmail.com>
2021-10-04 14:32:29 +0100

    Added annotations to Comment component

--- react-hooks-course-curriculum/app/app.tsx
+++ react-hooks-course-curriculum/app/app.tsx
@@ -3,12 +3,15 @@ import './index.css'
 import { Route, Switch } from 'react-router-dom'
 import Loading from './components/Loading'
 import Nav from './components/Nav'
+import { useThemeContext } from './contexts/theme'
 
 const Posts = React.lazy(() => import('./components/Posts'))
 const Post = React.lazy(() => import('./components/Post'))
 const User = React.lazy(() => import('./components/User'))
 
 export default function App() {
+  const { theme } = useThemeContext()
+
   return (
     <div className={theme}>
       <div className="container">
