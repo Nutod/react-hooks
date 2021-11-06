76f8fc3f997b48a6a2a56933058a0c37fbb3a179
Nutod <alukodotun@gmail.com>
2021-11-06 15:49:50 +0100

    Updated theme context api

--- react-hooks-course/app/contexts/theme.ts
+++ react-hooks-course/app/contexts/theme.ts
@@ -1,6 +1,20 @@
 import React from 'react'
+import generateContext from 'react-generate-context'
 
-const { Consumer, Provider } = React.createContext()
+const { Consumer, Provider } = React.createContext({})
 
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
+
+// Found the implementation I was looking for
+function useTheme() {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('light')
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'))
+  }, [])
+
+  return { theme, setTheme }
+}
+
+export const [ThemeContextProvider, useThemeContext] = generateContext(useTheme)
